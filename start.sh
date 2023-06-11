a5ecf27123dca4db9381e4471d334bfb928b5e8d
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-12 00:06:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/TechGiron/Guardian-Bot-V1 /Guardian-Bot-V1
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Guardian-Botapp
-fi
-cd /Guardian-Bot-V1
+echo "Cloning Repository"
+git clone https://github.com/TechGiron/Guardian-All-Shortner-BOT.git /Guardian-All-Shortner-BOT
+cd /Guardian-All-Shortner-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
