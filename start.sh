cc66651a03e45a5c59f65b4750a2452edecac93b
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-12 00:13:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repository"
-git clone https://github.com/TechGiron/Guardian-All-Shortner-BOT.git /Guardian-All-Shortner-BOT
-cd /Guardian-All-Shortner-BOT
+git clone https://github.com/TechGiron/Guardian-Bot-V1.git /Guardian-Bot-V1
+cd /Guardian-Bot-V1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
