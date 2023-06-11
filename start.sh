bf7323627cf96e00b2ac68f178698a0147523374
Rahul <111085537+TechGiron@users.noreply.github.com>
2023-06-11 23:46:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/TechGiron/Guardian-Bot-V1 /Guardian-Bot-V1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /Guardian-Bot-V1
 fi
-cd /Eva
+cd /Guardian-Bot-V1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
